dcba5ec334f37dcf23753f2e49b31229835209b7
AtomicLemon <46167928+AtomicLemon@users.noreply.github.com>
2021-05-25 01:39:30 +1200

    Update blockClient

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -28,7 +28,7 @@ export const v1Client = new ApolloClient({
 
 export const blockClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/blockartist/cheeseblock'
+    uri: 'https://api.thegraph.com/subgraphs/name/pancakeswap/blocks'
   }),
   cache: new InMemoryCache()
 })
